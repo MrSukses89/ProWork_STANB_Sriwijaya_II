76a21e244ae0ff054ef2a1ac8e158834ec9abba7
Mr. Sukses89 <andi8evo@gmail.com>
2026-05-18 10:11:49 +0800

    wallpaper fix

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -1,6 +1,6 @@
 // 1. Daftar Koleksi
 const koleksiWallpaper = [
-    "wallpaper/sr2.png",
+    
     "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&w=1920",
     "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?auto=format&fit=crop&w=1920",
     "https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?auto=format&fit=crop&w=1920",
@@ -10,6 +10,7 @@ const koleksiWallpaper = [
     "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1920",
     "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?auto=format&fit=crop&w=1920",
     "https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?auto=format&fit=crop&w=1920",
+    "wallpaper/sr2.png",
     "wallpaper/4.jpg"
    
    
